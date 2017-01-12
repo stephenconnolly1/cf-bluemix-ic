9adfa727c3991f5c735cb5dc0921e6fdff9a8e87
casey-capgem <casey.rogers@capgemini.com>
2017-01-12 14:25:50 +0000

    Update bluemix.sh

--- bluemix.sh
+++ bluemix.sh
@@ -1,15 +1,20 @@
-#!/bin/bash
-	
-function connect {
+#!/usr/bin/env bash
+
+VARS="CF_USER CF_PASS CF_ORG CF_SPACE CF_API CF_CONTAINER CF_PORTS LAUNCH_CMD"
+
+function checkvar {
+	if [ -z ${1} ];
+		then
+			echo "Variable ${1} is not set, please check your wercker environment variables."
+		else
+			return 0;
+	fi
+}
+
+function cfinit {
 	cf login -u $CF_USER -p $CF_PASS -o $CF_ORG -s $CF_SPACE -a $CF_API
 	cf ic init
 }
-
-connect
-
-IMAGE_NAME=${CF_CONTAINER}
-CF_OUTPUT=$(cf ic ps --format 'table {{.ID}}|{{.Image}}|{{.Ports}}' |grep ${IMAGE_NAME})
-RUNNING_CONTAINER=$(echo "$CF_OUTPUT" | grep $IMAGE_NAME | cut -d '|' -f 1)
 
 function running {
 	echo RUNNING - $1
@@ -23,8 +28,16 @@ function running {
 
 function setip {
 	IP_ADDRESS=$(cf ic ip request -q)
-        cf ic ip bind $IP_ADDRESS $1
+	cf ic ip bind $IP_ADDRESS $1
 }
+
+function buildports {
+	IFS=',' read -ra PORTS <<< "$CF_PORTS"
+	for element in "${PORTS[@]}"; do
+        printf " -p $element "
+	done
+}
+
 
 function reprovision {
 	if [ -z "$3" ]; then
@@ -33,10 +46,19 @@ function reprovision {
 		IP_ADDRESS=$3	
 	fi
 	cf ic rm $2 --force
-	CONTAINERID=$(cf ic run -p $CF_PORT:$CF_PORT registry.eu-gb.bluemix.net/aie_london/$CONTAINER_NAME $LAUNCH_CMD)
+	CONTAINERID=$(cf ic run $(buildports) registry.eu-gb.bluemix.net/aie_london/$CF_CONTAINER $LAUNCH_CMD)
 	cf ic ip bind $IP_ADDRESS $CONTAINERID
 	return 0
 }
+
+for i in VARS; do checkvar $i; done
+
+cfinit
+
+IMAGE_NAME=${CF_CONTAINER}
+CF_OUTPUT=$(cf ic ps --format 'table {{.ID}}|{{.Image}}|{{.Ports}}' |grep ${IMAGE_NAME})
+RUNNING_CONTAINER=$(echo "$CF_OUTPUT" | grep $IMAGE_NAME | cut -d '|' -f 1)
+
 
 running ${CF_OUTPUT} ${RUNNING_CONTAINER}
 
